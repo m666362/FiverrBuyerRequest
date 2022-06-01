4748758297dd05cfa153f0362db96790bb6b42d3
Md. Rayhan <55645384+m666362@users.noreply.github.com>
2022-06-01 13:17:00 +0600

    cron job

--- test.js
+++ test.js
@@ -1,20 +1,57 @@
-function convertTime(text="") {
-    let days = 0;
-    if(text.toLowerCase().includes("days") || text.toLowerCase().includes("day")){
-        days += parseFloat(text);
-    }
-    if(text.toLowerCase().includes("hours") || text.toLowerCase().includes("hour")){
-        days += Math.ceil(parseFloat(text)/24);
-    }
-    if(text.toLowerCase().includes("months") || text.toLowerCase().includes("month")){
-        days += Math.ceil(parseFloat(text)*24);
-    }
-    if(text.toLowerCase().includes("years") || text.toLowerCase().includes("year")){
-        days += Math.ceil(parseFloat(text)*24*12);
-    }
-    return days;
-}
+// function convertTime(text="") {
+//     let days = 0;
+//     if(text.toLowerCase().includes("days") || text.toLowerCase().includes("day")){
+//         days += parseFloat(text);
+//     }
+//     if(text.toLowerCase().includes("hours") || text.toLowerCase().includes("hour")){
+//         days += Math.ceil(parseFloat(text)/24);
+//     }
+//     if(text.toLowerCase().includes("months") || text.toLowerCase().includes("month")){
+//         days += Math.ceil(parseFloat(text)*24);
+//     }
+//     if(text.toLowerCase().includes("years") || text.toLowerCase().includes("year")){
+//         days += Math.ceil(parseFloat(text)*24*12);
+//     }
+//     return days;
+// }
 
-console.log(convertTime("73 eee"));
+const { default: axios } = require("axios");
 
-// console.log(25/2);
+// console.log(convertTime("73 eee"));
+
+// // console.log(25/2);
+
+const URL = `https://contoso.communication.azure.com//sms?api-version=2021-03-07`;
+
+let headersList = {
+  Accept: "*/*",
+  "Content-Type": "application/json",
+};
+let reqOptions = {
+  url: URL,
+  method: "POST",
+  headers: headersList,
+  data: {
+    from: "+8801766324950",
+    smsRecipients: [
+      {
+        to: "+8801629549539",
+      },
+    ],
+    message: "Hello world!",
+    smsSendOptions: {
+      enableDeliveryReport: true,
+      tag: "testSmsRequest",
+    },
+  },
+};
+
+const res = axios(reqOptions)
+  .then((response) => {
+    console.log({ response });
+    return response;
+  })
+  .catch((error) => {
+    console.log({ error });
+    throw error;
+  });
